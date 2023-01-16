91af7ad9addde1d490e96f7123ba19149cdc0001
konojack <konojack@gmail.com>
2023-01-16 16:41:21 +0100

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,8 @@ const { usersReducer } = require('./slices/usersSlice');
 export const apiDomain =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3005'
-    : 'https://my-json-server.typicode.com/konojack/users-albums-photos-server-async-thunks-1';
+    : // : 'https://my-json-server.typicode.com/konojack/users-albums-photos-server-async-thunks-1';
+      'https://users-albums-photos-server-async-thunks-1.vercel.app';
 
 export const store = configureStore({
   reducer: {
