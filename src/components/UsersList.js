afa87e8ad498c36d87c14b7d5ed96e1d6b7ed35c
konojack <konojack@gmail.com>
2023-01-17 15:28:53 +0100

    better display of list

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -22,38 +22,33 @@ const UsersList = () => {
     createUser();
   };
 
+  let content;
   if (isLoadingUsers) {
-    return (
-      <div>
-        <Skeleton times={6} className="h-10 w-full" />
-      </div>
-    );
-  }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = users.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = users.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between m-3 items-center">
         <h1 className="m-2 text-xl">Users</h1>
         <Button onClick={handleUserAdd} loading={isCreatingUser} primary>
           + Add User
         </Button>
         {creatingUserError && 'Error creating user...'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 };
