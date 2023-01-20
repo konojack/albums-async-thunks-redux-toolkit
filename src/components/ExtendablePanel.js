64133465b783f6a434966bdc826e6a977ec097fa
konojack <konojack@gmail.com>
2023-01-20 12:08:28 +0100

    expand panel toogle

--- src/components/ExtendablePanel.js
+++ src/components/ExtendablePanel.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+
 const ExtendablePanel = ({ header, children }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="flex p-2 justify-between items-center">
         <div className="flex justify-between items-center">{header}</div>
+        <div onClick={handleClick} className="cursor-pointer">
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      {expanded && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 };
