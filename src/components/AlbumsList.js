0ca13a50bc735fa87955dfbbbf04fccb2408fdec
konojack <konojack@gmail.com>
2023-01-23 13:26:10 +0100

    listed all albums for user

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,15 +1,32 @@
 import { useFetchAlbumsQuery } from '../store';
+import ExtendablePanel from './ExtendablePanel';
+import Skeleton from './Skeleton';
 
 const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  console.log(
-    '🚀 ~ file: AlbumsList.js:5 ~ AlbumsList ~ data, error, isLoading',
-    data,
-    error,
-    isLoading
-  );
 
-  return <div>Albums for {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExtendablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExtendablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default AlbumsList;
