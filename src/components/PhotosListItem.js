8eba79f69da8e7ee59e572950b81eef1be2406f3
konojack <konojack@gmail.com>
2023-02-14 17:27:18 +0100

    handled photos and all actions

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,24 @@
-const PhotosListItem = () => {
-  return <div>PhotosListItem</div>;
+import { GoTrashcan } from 'react-icons/go';
+import { useRemovePhotoMutation } from '../store';
+
+const PhotosListItem = ({ photo }) => {
+  const [removePhoto] = useRemovePhotoMutation();
+
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
+
+  return (
+    <div className="cursor-pointer relative">
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
+      <div
+        className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80"
+        onClick={handleRemovePhoto}
+      >
+        <GoTrashcan className="text-3xl" />
+      </div>
+    </div>
+  );
 };
 
 export default PhotosListItem;
